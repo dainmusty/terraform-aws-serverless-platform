8cfd756ef0c7851afa6672a2ed89f8c8cd1f2390
dainusty <musodainkey@gmail.com>
2026-05-12 12:03:42 +0000

    Automated the API endpoint injection

--- Backend/GETmethod.py
+++ Backend/GETmethod.py
@@ -1,15 +1,41 @@
 import json
 import boto3
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Student-Details')
+
+
 def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
-    table = dynamodb.Table('Student-Details')
+    try:
 
-    response = table.scan()
-    data = response['Items']
+        response = table.scan()
 
-    while 'LastEvaluatedKey' in response:
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
-    return data
+        data = response['Items']
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(
+                ExclusiveStartKey=response['LastEvaluatedKey']
+            )
+
+            data.extend(response['Items'])
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(data)
+        }
+
+    except Exception as e:
+
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({
+                'error': str(e)
+            })
+        }
